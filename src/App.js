f2e048a2b3896b24d00a47f3646837dfdeb97912
Varinea <53873011+varinea@users.noreply.github.com>
2020-03-24 09:30:43 -0300

    Interactividad (#6)
    * commit rama develop
    
    * probando here react
    
    * mapa react
    
    * Map React
    
    * Mapa con React (#2)
    
    * probando here react
    
    * mapa react
    
    * Map React
    
    * Interactividad Zoom al mapa, apretar alt y mover el mapa para ver edificios
    
    * ordenando el code de interactividad
    
    * enter
    
    * script develop y ui
    
    * interacciones
    
    Co-authored-by: karmina-jc <karmina.jarac@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {DisplayMapClass} from './components/DisplayMapClass';
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+  <DisplayMapClass />
   );
-}
-
-export default App;
+  }
+  
+  export default App;
