c799d0a06c1af43774fc44a48285d09c84df880d
karmina-jc <karmina.jarac@gmail.com>
2020-04-03 09:56:48 -0300

    mapa final 2

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ FirebaseAppProvider
 } from 'reactfire';
 
 
-<<<<<<< HEAD
 ReactDOM.render((
   <FirebaseAppProvider firebaseConfig={firebaseConfig}>
     <Suspense fallback={'Conectando con la App...'}>
@@ -19,9 +18,3 @@ ReactDOM.render((
   ), document.getElementById('root'));
   
   serviceWorker.unregister();
-=======
-// Si desea que su aplicación funcione sin conexión y se cargue más rápido, puede cambiar
-// anular el registro () para registrar () a continuación. Tenga en cuenta que esto viene con algunas trampas.
-// Obtenga más información sobre los trabajadores de servicios: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
->>>>>>> 7b7fc0dec9e80c66f2b15d82bcbfb26449edd935
